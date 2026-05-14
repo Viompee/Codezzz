266885a3527c6c626ef478aab2cb7a42ac51dec1
Viompee <asoquee63@gmail.com>
2026-05-14 15:27:00 +0800

    Add files via upload
    Problem: Piattos at the Power-Up Shop
    Objective:
    Piattos has arrived at the shop to buy a power-up before a big boss fight. The shopkeeper needs a program that asks Piattos how many coins he has, and then tells him the best item he can afford. However, the shop's computer is old, and if someone types a word (like "fifty") instead of a number (like "50"), the program crashes! You need to fix this using a safety net.
    
    Given Details:
    
    The Items (from most to least expensive):
    •Super Star: 100 coins
    
    •Fire Flower: 50 coins
    
    •Super Mushroom: 20 coins

---
+++ Portfolio2.py
@@ -0,0 +1,23 @@
+#Get the input from the user
+# Ask Piattos for his coin count
+raw_input = input("How many coins do you have, Piattos? ")
+#Check if the input is a valid number
+# 1. The try / except Structure (Safety Net)
+try:
+    coins = int(raw_input)
+except:
+    coins = -1 
+#Then run the logic to pick a power-up
+# 2. Multi-way Decisions (if / elif / else) [cite: 507, 511]
+if coins == -1:
+    print("Error: Please enter a numeric value for your coins.")
+elif coins >= 100:
+    print("Owwww PALDOOOO! You can afford the Super Star!")
+elif coins >= 50:
+    print("Eyyy! You can afford the Fire Flower! That op asf.")
+elif coins >= 20:
+    print("Well, You can afford the Super Mushroom! Not that good but gets the job done. Good Luck!!")
+elif coins >= 0:
+    print("Sorry Piattos, you no no afford any power ups!")
+else:
+    print("Wait... how do you have negative coins?")
